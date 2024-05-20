400d35387b145db4f92637a4a01e13ec6e0397af
ali fadil <alifadil4141@gmail.com>
2024-05-21 01:01:51 +0300

    finally

--- FadeFunc.cpp
+++ FadeFunc.cpp
@@ -6,11 +6,11 @@ class CFadeFunc : public CBaseEntity
 {
 public:
 	void Spawn() override;
-	void Think() override;
 private:
 	float looper;
 	void FadeIn();
 	void FadeOut();
+	float test; 
 };
 
 LINK_ENTITY_TO_CLASS(fade_function, CFadeFunc)
@@ -27,23 +27,31 @@ void CFadeFunc::Spawn()
 	pev->movetype = MOVETYPE_FLY;
 	SetThink(&CFadeFunc::FadeIn);
 	pev->nextthink = gpGlobals->time;
+	
 }
 
-void CFadeFunc::Think()
-{
-	ALERT(at_console, "the universe is expanding");
-	pev->nextthink = gpGlobals->time + 1;
-}
 
 void CFadeFunc::FadeIn()
 {
+	CBaseEntity* player = CBaseEntity::Instance(g_engfuncs.pfnPEntityOfEntIndex(1));
+
+	auto distance = player->pev->origin - pev->origin;
+
+	pev->velocity = distance;
+
+	float nor = atan2(distance.x, distance.y) * -1;
+
+	ALERT(at_console, "value %f \n", nor);
+
+	pev->angles = Vector(0,(nor * 50) + 70, 0);
+
 	pev->renderamt++;
 
-	if (pev->renderamt >= 255)
-		SetThink(&CFadeFunc::FadeOut);
+	if (pev->renderamt >= 255) {}
+		//SetThink(&CFadeFunc::FadeOut);
 
 
-	pev->nextthink = gpGlobals->time + 0.001;
+	pev->nextthink = gpGlobals->time + 0.0001;
 }
 
 void CFadeFunc::FadeOut()
@@ -52,5 +60,5 @@ void CFadeFunc::FadeOut()
 	if (pev->renderamt <= 0)
 		SetThink(&CFadeFunc::FadeIn);
 
-	pev->nextthink = gpGlobals->time + 0.001;
+	pev->nextthink = gpGlobals->time + 0.0001;
 }
